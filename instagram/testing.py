06810869af002450e9cff6776561e580b92456fc
Priyanshu-Batham <priyanshubatham24733@gmail.com>
2023-01-17 19:53:43 +0530

    done

--- instagram/testing.py
+++ instagram/testing.py
@@ -1,24 +1,13 @@
 from instabot import Bot
 import time
 bot=Bot()
-bot.login(username="",password="")
+bot.login(username="theooperator",password="")
 
-'bot.follow("priyanshu_batham_1")'
-'print("followed successfully")'
-
-
-
-'''bot.like_user(user_id="priyanshu_batham_1",filtration= False)
-print("Likeddddd")'''
-
-bot.follow_following(user_id="priyanshu_batham_1")
+bot.follow("priyanshu_batham_1")
+print("followed successfully")
 
 
-'''a=bot.get_user_following(user_id="priyanshu_batham_1",nfollows=5)
-b=[]
-for i in a:
-    b.append(bot.get_username_from_user_id(i))
-print(b)'''
+
 
 
 
